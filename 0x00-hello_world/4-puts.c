2a8e15e2c60d9f62566f6b6a78ba55559e4d3fff
Jocelyn Vega <jossvega103@gmail.com>
2020-06-11 01:45:09 +0000

    Modified task 4 one more time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,4 @@
 #include <stdio.h>
- main()
- {
- printf("hello, world\n");
-#!/bin/bash
-#include <stdio.h>
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-#include <stdio.h>
 /**
  * main - code to put a message
  * Return: Always 0
