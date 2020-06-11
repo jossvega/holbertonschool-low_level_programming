52c63d8217a589a1d185924bf744d6000ee9cb2c
Jocelyn Vega <jossvega103@gmail.com>
2020-06-11 01:08:11 +0000

    Task from 0 to 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ main()
+ {
+ printf("hello, world\n");
+#!/bin/bash
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
