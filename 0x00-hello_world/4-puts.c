57f18ec32ad70857992d8b4990ab73b0824f6fca
Jocelyn Vega <jossvega103@gmail.com>
2020-06-11 01:38:24 +0000

    Modified task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,4 +8,14 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+#include <stdio.h>
+/**
+ * main - code to put a message
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
