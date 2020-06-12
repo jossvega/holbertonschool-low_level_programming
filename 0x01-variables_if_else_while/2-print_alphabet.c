88763fecd0a3859ee1c780400a8d2f308cfc27a6
Jocelyn Vega <jossvega103@gmail.com>
2020-06-12 04:49:27 +0000

    modified task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
-  *main-random number is positive, negative or zero
-  *
-  *Return:0
-  */
-
+* main-random number is positive, negative or zero
+*
+* Return:0
+*/
 int main(void)
 {
 	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar (ch);
+	for (ch='a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
 	return (0);
 }
 	
