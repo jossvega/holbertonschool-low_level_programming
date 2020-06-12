765edbc5c7c07c0a67f3c41cf9ad5802de1aa540
Jocelyn Vega <jossvega103@gmail.com>
2020-06-12 05:40:33 +0000

    modified task 0

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
-* main - For loop that prints out entire alphabet using putchar
+* main - Demonstrates if...else if...else statements
 *
 * Return: 0
 */
 int main(void)
 {
-	char alphabet;
-
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		putchar(alphabet);
-	putchar('\n');
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
 	return (0);
 }
