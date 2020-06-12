352a593778f27301253e014e0fa890a8693a5fe9
Jocelyn Vega <jossvega103@gmail.com>
2020-06-12 05:17:39 +0000

    modified task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-   * main - For loop that prints out entire alphabet using putchar
-    *
-     * Return: 0
-      */
+* main - For loop that prints out entire alphabet using putchar
+*
+* Return: 0
+*/
 int main(void)
 {
 	char alphabet;
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
 	putchar('\n');
