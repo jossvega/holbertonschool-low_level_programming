30ad10b39b894b38c573517c191e04db63f243d2
Jocelyn Vega <jossvega103@gmail.com>
2020-06-15 15:41:35 +0000

    advance modified 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,36 +6,29 @@
  */
 int main(void)
 {
-	int n1;
-	int n2;
-	int n3;
-	int n4;
+	int number1;
+	int number2;
 
-	for (n1 = 48 ; n1 <= 57 ; n1++)
+	for (number1 = 0; number1 <= 99; number1++)
 	{
-		for (n2 = 48 ; n2 <= 57 ; n2++)
+		for (number2 = 0; number2 <= 99; number2++)
 		{
-			for (n3 = 48 ; n3 <= 57 ; n3++)
+			if (number1 < number2)
 			{
-				for (n4 = 48 ; n4 <= 57 ; n4++)
+				putchar ((number1 / 10) + 48);
+				putchar ((number1 % 10) + 48);
+				putchar (' ');
+				putchar ((number2 / 10) + 48);
+				putchar ((number2 % 10) + 48);
+
+				if (number1 != 98 || number2 != 99)
 				{
-					if (n2 < n4)
-					{
-						putchar(n1);
-						putchar(n2);
-						putchar(' ');
-						putchar(n3);
-						putchar(n4);
-						if (n2 != 56 || n3 != 57)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					putchar (',');
+					putchar (' ');
 				}
 			}
 		}
 	}
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
