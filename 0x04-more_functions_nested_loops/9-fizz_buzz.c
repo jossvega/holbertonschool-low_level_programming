12b9204a63cd9d6b3992f285cdb9d7f4159cb0ca
Jocelyn Vega <jossvega103@gmail.com>
2020-06-19 04:36:59 +0000

    Task 9.5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,30 +10,29 @@
 int main(void)
 {
 
-	int j;
+        int j;
 
-	for (j = 1; j <= 100; j++)
-	{
-		if ((j % 3 == 0) && (j % 5 == 0))
-		{
-			printf("Fizz Buzz");
-		}
-		else if ((j % 3 == 0) && (j % 5 != 0))
-		{
-			printf(" Fizz");
-		}
-		else if ((j % 5 == 0) && (j % 3 != 0))
-		{
-			printf("Buzz ");
-		}
-		else if ( j == 100)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf(" %d ", j);
-		}
-	}
-	printf("\n");
+        for (j = 1; j <= 100; j++)
+        {
+                if (j % 3 == 0 && j % 5 == 0)
+                {
+                        printf("FizzBuzz");
+                }
+                else if (j % 3 == 0 && j % 5 != 0)
+                {
+                        printf("Fizz");
+                }
+                else if (j % 5 == 0 && j % 3 != 0)
+                {
+                        printf("Buzz");
+                }
+                else
+                {
+                        printf("%d", j);
+                }
+
+                if (j != 100)
+                        printf(" ");
+        }
+        printf("\n");
 }
