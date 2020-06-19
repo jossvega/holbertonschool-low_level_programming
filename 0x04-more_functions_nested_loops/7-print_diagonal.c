d92b648e48290ca3c1efa0646bfd002acb5fbb88
Jocelyn Vega <jossvega103@gmail.com>
2020-06-19 04:55:58 +0000

    task 7.5

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,12 +16,18 @@ void print_diagonal(int n)
 		for (j = 0;j <= i; j++)
 		{
 			if (j != i)
+			{
 				_putchar(' ');
+			}
 			else
+			{
 				_putchar('\\');
+			}
 		}
 		_putchar('\n');
 	}
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 }
