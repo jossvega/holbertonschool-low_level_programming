ed6cd768922e4d5c2cd09c42b9cd55d8b16020f7
Jocelyn Vega <jossvega103@gmail.com>
2020-06-21 04:26:55 +0000

    Advance 101 prints_integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ * Return: void.
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		x = -n;
+		_putchar('-');
+	} else
+	{
+		x = n;
+	}
+
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+
+	_putchar((x % 10) + '0');
+}
