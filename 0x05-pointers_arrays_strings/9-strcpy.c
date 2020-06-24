eb7a787eb356397f9a752532cd5e32c057818764
Jocelyn Vega <jossvega103@gmail.com>
2020-06-24 01:02:56 +0000

    Task 9 strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies the string pointed to by src and null
+ * @dest: elements of arrays s1.
+ * @src: pointer to string source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
