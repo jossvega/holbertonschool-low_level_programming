bda553646c51f34bd18ecfea839d8f2d788fd655
Jocelyn Vega <jossvega103@gmail.com>
2020-06-24 01:06:17 +0000

    Task 9.1 strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,8 +11,9 @@ char *_strcpy(char *dest, char *src)
 	int x;
 
 	for (x = 0; src[x] != '\0'; x++)
-	{
 		dest[x] = src[x];
-	}
+
+	for (; dest[x] != '\0'; x++)
+		dest[x] = '\0';
 	return (dest);
 }
