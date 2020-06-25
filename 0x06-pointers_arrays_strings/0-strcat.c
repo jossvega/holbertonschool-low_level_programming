e4634a9f88c468ede4cc8a10b1b51735276292fa
Jocelyn Vega <jossvega103@gmail.com>
2020-06-25 18:18:17 +0000

    Task 0.1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
- * _strcpy - copies the string pointed to by src and null
- * @dest: elements of arrays s1.
- * @src: pointer to string source
- * Return: dest
+ * _strcat- function that concatenates two strings.
+ *
+ * @dest: pointer to destination char
+ * @src: pointer to source char
+ * Return: char
  */
 
 char *_strcat(char *dest, char *src)
 {
-	unsigned int d = 0;
-	unsigned int s = 0;
+	int d = 0;
+	int s = 0;
 
 	while (dest[d] != '\0')
 	{
