04969effbb31be4145e4b6175e52188a689f8bfa
Jocelyn Vega <jossvega103@gmail.com>
2020-06-25 20:48:31 +0000

    Task 1 strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strcat- function that concatenates two strings.
+ *
+ * @dest: pointer to destination char
+ * @src: pointer to source char
+ * @n: it will use at most n bytes from src
+ * Return: char
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	unsigned int d = 0;
+	int s = 0;
+
+	while (dest[d] != '\0')
+	{
+		d++;
+	}
+	while (s < n && src[s] != '\0')
+	{
+		dest[d] = src[s];
+		d++;
+		s++;
+	}
+	return (dest);
+}
