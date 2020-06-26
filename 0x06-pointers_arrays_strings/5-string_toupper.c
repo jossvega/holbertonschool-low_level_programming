1cebd2589acb0cb80462f2543efc8eb8fa595752
Jocelyn Vega <jossvega103@gmail.com>
2020-06-26 02:05:51 +0000

    Task 5 toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *string_toupper(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] >= 'a' && s[x] <= 'z')
+			s[x] = s[x] - 32;
+	}
+
+	return (s);
+}
