e63e531a95a9dd8cc638031bf6dc328d4c8255af
Jocelyn Vega <jossvega103@gmail.com>
2020-06-28 21:12:03 +0000

    Task 2 strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strchr - function that locates a character in a string.
+ *
+ * @s: string to receive and return
+ * @c: character to receive
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	
+	return (NULL);
+}
