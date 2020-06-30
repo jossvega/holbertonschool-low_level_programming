b6dd83147c0b00e1094c48e7d72726a4e47d63dd
Jocelyn Vega <jossvega103@gmail.com>
2020-06-30 03:57:17 +0000

    Task 9.1 set string and update file holberton.h

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -2,8 +2,8 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer we need to set to
- * @to: string to set
+ * @s: pointer to pointer we need to set.
+ * @to: string to set.
  */
 void set_string(char **s, char *to)
 {
