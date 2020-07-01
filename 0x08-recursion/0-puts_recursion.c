16f5a6f66441052dc7f60a9dde5390d46fd5cd35
Jocelyn Vega <jossvega103@gmail.com>
2020-07-01 20:20:46 +0000

    Task 0 prints a string, followed by a new line.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion -  function that prints a string
+ *
+ * @s: string to receive
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
