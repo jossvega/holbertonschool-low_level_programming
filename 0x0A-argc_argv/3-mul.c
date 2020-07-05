252667f8890c821f5cef7210e9f3d5fe70ad5c4f
Jocelyn Vega <jossvega103@gmail.com>
2020-07-05 03:09:16 +0000

    task 3.2 Write a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,12 +6,12 @@
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: Always o.
+ * Return: 0, 1.
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc > 1)
+	if (argc == 1)
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
