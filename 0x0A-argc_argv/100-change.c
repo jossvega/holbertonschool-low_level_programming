4186e36983375cbbdfbcefeade78d2174a16db63
Jocelyn Vega <jossvega103@gmail.com>
2020-07-05 06:37:39 +0000

    Advance 100 program that prints the minimum number of coins to make change for an amount of money.

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - minimum number of coins to
+ * make change for an amount of money
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int num, x, min;
+	int mon[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	min = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (x = 0; x < 5; x++)
+	{
+		while (num >= mon[x])
+		{
+			num = num - mon[x];
+			min++;
+		}
+	}
+	return (0);
+}
