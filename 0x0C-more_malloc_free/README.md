f05be977c80a55b3646c0f14e467bf42ef58ea05
Jocelyn Vega <jossvega103@gmail.com>
2020-07-09 21:29:22 +0000

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,5 @@
+**x0C. C - More malloc, free**
+
+* Task 0. Trust no one
+* Task 1. string_nconcat
+* Task 2. _calloc
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
