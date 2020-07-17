4555f6794f4fd78cbdac28a0c10c8e05543bb8bc
Jocelyn Vega <jossvega103@gmail.com>
2020-07-17 02:17:48 +0000

    Task 3-2 modified if

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -22,9 +23,10 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
+	
 	while (i < 5)
 	{
-		if (*s == *(ops[i]).op)
+		if (strcmp(s, (ops[i].op)) == 0)
 		{
 			return (*(ops[i]).f);
 		}
