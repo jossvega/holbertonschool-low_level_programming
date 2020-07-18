ba87c239a9385082a195815e2d8fb18196c916b5
Mona Lisa <jossvega103@gmail.com>
2020-07-18 18:23:46 +0000

    Task 2.2 (Add new conditional "nil") Write a function that prints strings, followed by a new line.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,8 +16,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (x = 0; x < n; x++)
 	{
 		let = va_arg(name, char*);
+		if (let == NULL)
+			printf("(nil)");
 		if (x < (n - 1) && separator != 0)
-			printf("%s%s", let,separator);
+			printf("%s%s", let, separator);
 		else
 			printf("%s", let);
 	}
