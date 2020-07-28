220712895166b9cd99a66389acd61cfa76c5f23d
Jocelyn Vega <jossvega103@gmail.com>
2020-07-28 01:27:43 +0000

    Task 0 - Write a function that prints all the elements of a listint_t list, file README and lists.h

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t cont = 0;
+
+	while (h)
+	{
+		printf("%d\n", (*h).n);
+		h = (*h).next;
+		cont++;
+	}
+	return (cont);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,13 @@
+**0x13. C - More singly linked lists**
+
+* Task 0.Print list
+* Task 1. List length
+* Task 2. Add node
+* Task 3. Add node at the end
+* Task 4. Free list
+* Task 5. Free
+* Task 6. Pop
+* Task 7. Get node at index
+* Task 8. Sum list
+* Task 9. Insert
+* Task 10. Delete at index
