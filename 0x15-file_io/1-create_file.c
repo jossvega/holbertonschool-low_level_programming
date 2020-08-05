c4538f695edd94f220f02d5ae0423edf42d49417
Jocelyn Vega <jossvega103@gmail.com>
2020-08-05 03:41:22 +0000

    Task 1 Create a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file and fills it with text
+ * @filename: name of the file to create
+ * @text_content: text to write in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, str = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[str])
+			str++;
+		wr = write(fd, text_content, str);
+		if (wr != str)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
