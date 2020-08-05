adfb1f59c9277703668969294c101ea96f51afdc
Jocelyn Vega <jossvega103@gmail.com>
2020-08-05 01:29:30 +0000

    Task 0 - Write a function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: file to read
+ * @letters: number of letters it should read and print
+ * Return: actual numbers it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, wri, rd;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
+
+	fd = open(filename, O_RDWR);
+
+	if (fd == -1)
+		return (0);
+
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+		return (0);
+
+	wri = write(STDOUT_FILENO, buff, rd);
+	if (wri == -1)
+		return (0);
+
+	close(fd);
+	free(buff);
+	return (wri);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+**0x15. C - File I/O**
+* Task 0. Tread lightly, she is near
+* Task 1. Under the snow
+* Task 2. Speak gently, she can hear
+* Task 3. cp
