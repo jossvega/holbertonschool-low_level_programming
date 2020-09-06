45883c2266fd11e0811096da3698274ab3240e8d
Jocelyn Vega <jossvega103@gmail.com>
2020-09-06 18:42:55 +0000

    Add description for funtction print_dlistint

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_listint - prints all the elements of a linked list
+ * print_dlistint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
