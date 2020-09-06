db3308140a5db68af2af19c12afdb6e17afe177b
Jocelyn Vega <jossvega103@gmail.com>
2020-09-06 19:30:50 +0000

    Modify prototype

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * print_dlistint - prints all the elements of a linked list
+ *  dlistint_len - returns the number of elements in a linked dlistint_t list.
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t cont = 0;
 
