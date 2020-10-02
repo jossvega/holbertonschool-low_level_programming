18ce8ce84bb2dd2a1fe2068afe3478d38aad03c0
Jocelyn Vega <jossvega103@gmail.com>
2020-10-02 18:05:59 +0000

    Task 2.1 - Betty style

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,7 +6,7 @@
  * @size: size of the array of the hash table
  * Return: the index at which the key/value pair should be stored
  * in the array of the hash table
- */	
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
