a93ffe9fc0f13d89c856dc2cd3c6c2e41f2a7200
Jocelyn Vega <jossvega103@gmail.com>
2020-11-08 03:38:43 +0000

    Agregar un nuevo método y modificar la lógica del perímetro

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,21 +2,33 @@
 """Program that discover the perimeter of an island depends on grid"""
 
 
+def count_edge(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    edge = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        edge += 1
+    if j <= 0 or not grid[i][j - 1]:
+        edge += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        edge += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        edge += 1
+
+    return edge
+
+
 def island_perimeter(grid):
+    """Returns the perimeter of the island in grid."""
 
     perim = 0
-    col = len(grid)
+    r = len(grid)
 
-    for i in range(col):
-        row = len(grid[i])
-        for j in range(row):
+    for i in range(r):
+        c = len(grid[i])
+        for j in range(c):
             if grid[i][j] == 1:
-                if grid[i][j - 1] == 0:
-                    perim += 1
-                if grid[i - 1][j] == 0:
-                    perim += 1
-                if grid[i][j + 1] == 0:
-                    perim += 1
-                if grid[i + 1][j] == 0:
-                    perim += 1
+                perim += count_edge(grid, i, j)
+
     return perim
